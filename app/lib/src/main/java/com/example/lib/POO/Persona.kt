902e0ae75587c52369f2b0c474e16b11bffa8f80
YgnacioDelRio <arturoy.delrio@gmail.com>
2026-02-26 16:03:23 -0600

    Persona

---
+++ app/lib/src/main/java/com/example/lib/POO/Persona.kt
@@ -0,0 +1,22 @@
+package com.example.lib.POO
+
+class Persona(val nombre: String, val edad: Int, val ciudad: String) {
+    fun saludar() {
+        // TODO: Imprime el saludo
+        println("Hola, me llamo $nombre y tengo $edad años.")
+    }
+    fun esMayorDeEdad(): Boolean {
+        // TODO: Regresa true si edad >= 18
+        return edad >= 18
+    }
+}
+fun main() {
+    val persona1 = Persona("Ana", 20, "Guadalajara")
+    val persona2 = Persona("Luis", 15, "Leon")
+    // TODO: Llama los métodos de cada persona
+    persona1.saludar()
+    println(persona1.esMayorDeEdad())
+    println("------")
+    persona2.saludar()
+    println(persona2.esMayorDeEdad())
+}
