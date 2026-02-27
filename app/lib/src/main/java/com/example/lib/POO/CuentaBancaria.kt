f9e82304e4501f4eb0466ba9af57b24f82688509
YgnacioDelRio <arturoy.delrio@gmail.com>
2026-02-27 11:34:20 -0600

    CuentasBancarias

---
+++ app/lib/src/main/java/com/example/lib/POO/CuentaBancaria.kt
@@ -0,0 +1,32 @@
+package com.example.lib.POO
+
+class CuentaBancaria(val titular: String) {
+    var saldo: Double = 0.0
+    fun depositar(cantidad: Double) {
+        // TODO
+        saldo += cantidad
+        println("El nuevo saldo es: $saldo")
+    }
+    fun retirar(cantidad: Double) {
+        // TODO: verifica que haya saldo suficiente
+        var suma: Double = 0.0
+        suma = saldo - cantidad
+        if (suma >= 0){
+            saldo = suma
+            println("El nuevo saldo es: $saldo")
+        }else{
+            println("La cuenta no tiene suficientes fondos")
+        }
+    }
+    fun mostrarSaldo() {
+        // TODO
+        println("El saldo actual es: $saldo")
+    }
+}
+fun main() {
+    val cuenta = CuentaBancaria("María García")
+    cuenta.depositar(1000.0)
+    cuenta.retirar(400.0)
+    cuenta.retirar(800.0) // No debe permitirlo
+    cuenta.mostrarSaldo()
+}
